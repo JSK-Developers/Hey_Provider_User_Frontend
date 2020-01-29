e781f8e6aed3b51ba9a548758c2e928fe51acb3d
JSK-Developers <55729000+JSK-Developers@users.noreply.github.com>
2020-01-29 18:36:04 +0530

    varshil
    modal update

--- src/app/customer-services/customer-service-models/customer-services-model.component.ts
+++ src/app/customer-services/customer-service-models/customer-services-model.component.ts
@@ -1,21 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { BsModalRef } from 'ngx-bootstrap';
+import {  OnInit } from '@angular/core';
 
+import { Component, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 @Component({
     selector: 'app-customer-service-model',
     templateUrl: './customer-services-model.component.html',
     styleUrls: ['./customer-services-model.component.scss']
 })
-export class CustomerServiceModelComponent implements OnInit {
+export class CustomerServiceModelComponent {
 
     title: string;
     closeBtnName: string;
     list: any[] = [];
-
-    constructor(public bsModalRef: BsModalRef) { }
-
-    ngOnInit() {
-        this.list.push('PROFIT!!!');
+    modalRef: BsModalRef | null;
+    modalRef2: BsModalRef;
+    constructor(private modalService: BsModalService) {}
+   
+    openModal(template: TemplateRef<any>) {
+      this.modalRef = this.modalService.show(template, { class: 'modal-sm' });
     }
+    openModal2(template: TemplateRef<any>) {
+      this.modalRef2 = this.modalService.show(template, { class: 'second' });
+    }
+    closeFirstModal() {
+      if (!this.modalRef) {
+        return;
+      }
+   
+      this.modalRef.hide();
+      this.modalRef = null;
+    }
+  }
+    //constructor(public bsModalRef: BsModalRef) { }
 
-}
+   
+
+
+    
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, Inject, HostListener } from '@angular/core';
+import { Inject, HostListener } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap';
+import { CustomerServiceModelComponent } from '..//customer-services/customer-service-models/customer-services-model.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,12 +10,18 @@ import { Component, OnInit, Inject, HostListener } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
+  bsModalRef: BsModalRef;
   // isUserLoggidIn: boolean = false
-  constructor() { }
+  constructor(private modalService: BsModalService, public router: Router) { }
 
   ngOnInit() {
     // this.isUserLoggidIn = this.hardcodedAuthenticationervice.isUserLoggedIn();
+  }
+  openModalWithComponent(template: TemplateRef<any>) {
+    //   this.router.navigate(['services']);
+
+    this.bsModalRef = this.modalService.show(CustomerServiceModelComponent, Object.assign({}, { class: 'gray modal-lg' }));
+    this.bsModalRef.content.closeBtnName = 'Close';
   }
 
 
